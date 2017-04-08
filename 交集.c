56f95f9c920fe46bdf186348f8f3f6ed7f7e0331
westthunder <1031255818@qq.com>
2017-04-08 22:41:35 +0800

    交集

---
+++ 交集.c
@@ -0,0 +1,73 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct linked *lklist;     //定义链表 
+struct linked
+{
+	int key;
+	int *link;
+};
+
+Readlist()
+{
+	lklist lk,rear,t;
+	lk = (lklist)malloc(sizeof(struct linked));
+	
+	lk->link = NULL;
+	rear = lk;
+	
+	int num,k;
+	printf("Please input the number of the linked list:\n");
+	scanf("%d",&num);
+	printf("Now input the numbers:\n");
+	while(num--)
+	{
+		scanf("%d",&k);
+		Attach(k,&rear);
+	}
+	
+	t = lk;
+	lk = lk->link;
+	free(t);
+		
+	return lk;
+	
+}
+
+Compare(lklist l1,lklist l2)
+{
+	while(l1 && l2){
+		if(l1->key < l2->key )
+		{
+			l1 = l1->link;
+		} else if (l1->key > l2->key)
+		{
+			l2 = l2->link;
+		} else
+		{
+			printf("%d\t",l1->key);
+			l1 = l1->link;
+			l2 = l2->link;
+		}
+	}
+}
+
+Attach(int k, lklist *pRear)
+{
+	lklist l;
+	l = (lklist)malloc(sizeof(struct linked));
+	l->key  = k;
+	l->link  = NULL;
+	(*pRear)->link = l;
+	*pRear = l;	
+}
+
+
+int main(void)
+{
+	lklist lk1,lk2;
+	lk1 = Readlist();
+	lk2 = Readlist();
+	Compare(lk1,lk2);
+	return 0;
+}
