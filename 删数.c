05ff4c581dcc6f344e8b4f61ec2685d81865afb1
westthunder <jianhaoliu17@163.com>
2017-04-10 13:01:17 +0800

    删数

--- 删数.c
+++ 删数.c
@@ -1,7 +1,7 @@
-/* ��Ŀ������
- * ����һ������Ĵ�Сsize���ҳ�ʼ��Ϊ0��1��2...size-1
- * ÿ��������ɾȥһ����ֱ��ʣ�����һ����
- * ��ӡ���һ����
+﻿/* 题目描述：
+ * 输入一个数组的大小size，且初始化为0，1，2...size-1
+ * 每个两个数删去一个数直到剩下最后一个数
+ * 打印最后一个数
  */
  
 #include<stdio.h>
@@ -12,27 +12,27 @@ int main(void)
 	scanf("%d",&size);
 	int a[size];
 	
-	//��ʼ������
+	//初始化数组
 	int i;
 	for(i = 0; i < size; i++)
 	{
 		a[i] = i;
 	} 
 	
-	//ɾ��
+	//删除
 	int count = 0, rest = size;
 	while(rest != 1)
 	{
 		for(i = 0; i < size;)
 		{
 			
-			if(count == 2 && a[i] != -1)  //ɾ�� 
+			if(count == 2 && a[i] != -1)  //删数 
 			{
 				a[i] = -1;
 				rest--;
 				count = 0;
 				i++;
-			} else if(a[i] == -1 )  //�����ɾ����־������������ 
+			} else if(a[i] == -1 )  //如果有删除标志则跳过不计数 
 			{
 				i++;
 			} else                 
@@ -43,7 +43,7 @@ int main(void)
 		}
 	}
 	
-	//��ӡʣ�µ����һ���� 
+	//打印剩下的最后一个数 
 	for(i = 0; i < size; i++)
 	{
 		if(a[i] != -1)
